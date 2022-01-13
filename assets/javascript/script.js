1b6261649393dea71517c25b6707fb339333dca6
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-01-13 20:40:19 +0000

    add js structure

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,8 @@ let startButton = document.getElementById('start-btn');
 startButton.addEventListener('click', quizStart)
 
 function quizStart(){
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide')
 
 }
 
